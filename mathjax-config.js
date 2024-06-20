49c7ba9b7f5534c3b4a26e68ae243f2d9d45e2cb
Dibakar Sigdel <sigdeldkr@gmail.com>
2024-06-20 16:52:50 -0700

    logo

--- mathjax-config.js
+++ mathjax-config.js
@@ -1,10 +1,21 @@
 window.MathJax = {
   tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true
   },
   options: {
     skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
-    processHtmlClass: 'arithmatex'  // Only process elements with the "arithmatex" class
+    processHtmlClass: 'arithmatex',  // Only process elements with the "arithmatex" class
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
   }
 };
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
